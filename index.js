005eb4820d03c08fc43d70de3cf811ede74cf57f
PoonamRai23 <128670417+PoonamRai23@users.noreply.github.com>
2023-07-18 18:05:48 +0530

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const express = require('express');
+const express= require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -17,3 +16,4 @@ app.post('/product', (req, res, next) => {
 app.use('/', (req, res, next) => {
   res.send('<h1>Hello from Express!</h1>');
 });
+app.listen(3000);
